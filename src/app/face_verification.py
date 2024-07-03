a5f3992aee519b4bac6712f8cdb58ded5508f2b8
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-07-03 04:17:20 +0000

    changes in .py files

--- src/app/face_verification.py
+++ src/app/face_verification.py
@@ -1,23 +1,30 @@
-import os
 from deepface import DeepFace
 
 class FaceVerificationService:
+    """
+    Сервис верификации лиц.
+    """
+
     def __init__(self):
-        self.model_name = "Facenet"
+        """
+        Конструктор класса.
+        """
+
+        self.model_name = 'Facenet'
         self.model = DeepFace.build_model(self.model_name)
 
-    def generate_face_vector(self, img_path: str):
+    def generate_face_vector(self, img_path: str) -> list:
         """
-        Метод для генерации вектора лица на основе фотографии
+        Метод для генерации вектора лица на основе фотографии.
         """
+
         try:
+            # Метод represent возвращает список, берем первый элемент из списка
             face = DeepFace.represent(
                 img_path, model_name=self.model_name, enforce_detection=True)[0]
-            vectors = face["embedding"]
+            vectors = face['embedding']
 
             return vectors
 
         except Exception as e:
-            print(f"Ошибка при обработке изображения: {e}")
-            return None
-
+            return f'Ошибка при обработке изображения: {e}'
--- src/app/main.py
+++ src/app/main.py
@@ -1,19 +1,27 @@
 import logging
+
 from app.face_verification import FaceVerificationService
 
-def get_greetings() -> str:
-    """Возвращает строку приветствия."""
-    return 'Hello World!'
+
+def main():
+    """
+    Основная функция для выполнения верификации лиц на изображениях.
+    """
+
+    service = FaceVerificationService()
+    img_path1 = 'src/tests/img/1.jpg'
+    img_path2 = 'src/tests/img/2.jpg'
+    img_path3 = 'src/tests/img/3.jpg'
+    face_vector = service.generate_face_vector(img_path1)
+    logging.info('Вектор 1-го изображения')
+    logging.info(face_vector)
+    face_vector = service.generate_face_vector(img_path2)
+    logging.info('Вектор w-го изображения')
+    logging.info(face_vector)
+    face_vector = service.generate_face_vector(img_path3)
+    logging.info('Вектор e-го изображения')
+    logging.info(face_vector)
 
 
 if __name__ == '__main__':
-    service = FaceVerificationService()
-    img_path_1 = "src/tests/img/1.jpg"
-    img_path_2 = "src/tests/img/2.jpg"
-    img_path_3 = "src/tests/img/3.jpg"
-    face_vector = service.generate_face_vector(img_path_1)
-    print(face_vector)
-    face_vector = service.generate_face_vector(img_path_2)
-    print(face_vector)
-    face_vector = service.generate_face_vector(img_path_3)
-    print(face_vector)
+    main()
