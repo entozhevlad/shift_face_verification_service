e502a62924f9eb3f4407136ca88d0d66b6ad51c4
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-07-14 04:24:03 +0000

    Изменения по линтеру

--- src/app/face_verification.py
+++ src/app/face_verification.py
@@ -9,15 +9,18 @@ class FaceVerificationService:
         self.model_name = 'Facenet'
         self.model = DeepFace.build_model(self.model_name)
 
-    def generate_face_vector(self, img_path: str) -> list:
+    def generate_face_vector(self, img: str) -> list:
         """Метод для генерации вектора лица на основе фотографии."""
+        model = self.model_name
         try:
             # Метод represent возвращает список, берем первый элемент из списка
             face = DeepFace.represent(
-                img_path, model_name=self.model_name, enforce_detection=True)[0]
-            vectors = face['embedding']
+                img,
+                model_name=model,
+                enforce_detection=True,
+            )[0]
 
-            return vectors
+        except Exception as exc:
+            return 'Ошибка при обработке изображения: {0}'.format(exc)
 
-        except Exception as e:
-            return f'Ошибка при обработке изображения: {e}'
+        return face['embedding']
--- src/app/main.py
+++ src/app/main.py
@@ -5,19 +5,18 @@ from app.face_verification import FaceVerificationService
 
 def main():
     """Основная функция для выполнения верификации лиц на изображениях."""
+    # Настраиваем логгер
+    logging.basicConfig(level=logging.DEBUG)
+
     service = FaceVerificationService()
-    img_path1 = 'src/tests/img/1.jpg'
-    img_path2 = 'src/tests/img/2.jpg'
-    img_path3 = 'src/tests/img/3.jpg'
-    face_vector = service.generate_face_vector(img_path1)
-    logging.info('Вектор 1-го изображения')
-    logging.info(face_vector)
-    face_vector = service.generate_face_vector(img_path2)
-    logging.info('Вектор 2-го изображения')
-    logging.info(face_vector)
-    face_vector = service.generate_face_vector(img_path3)
-    logging.info('Вектор 3-го изображения')
-    logging.info(face_vector)
+    img = ['src/tests/img/1.jpg', 'src/tests/img/2.jpg', 'src/tests/img/3.jpg']
+
+    face_vector = service.generate_face_vector(img[0])
+    logging.info('Вектор {0}-го изображения: {1}'.format(1, face_vector))
+    face_vector = service.generate_face_vector(img[1])
+    logging.info('Вектор {0}-го изображения: {1}'.format(2, face_vector))
+    face_vector = service.generate_face_vector(img[2])
+    logging.info('Вектор {0}-го изображения: {1}'.format(3, face_vector))
 
 
 if __name__ == '__main__':
