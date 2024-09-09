f683977dac6426681660e11f576cbd969a31e84d
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-09-09 23:32:47 +0700

    Исправлен Jaeger

--- src/app/main.py
+++ src/app/main.py
@@ -1,20 +1,29 @@
 from fastapi import FastAPI
 from opentelemetry import trace
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.jaeger.proto.grpc import JaegerExporter
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from src.app.routers import face_verification
 
 app = FastAPI()
 
-# Настройка трейсера
-trace.set_tracer_provider(TracerProvider())
+# Настройка ресурса с указанием имени сервиса
+resource = Resource.create(attributes={"service.name": "face_verification_service"})
+
+# Инициализация трейсера с ресурсом
+trace_provider = TracerProvider(resource=resource)
+trace.set_tracer_provider(trace_provider)
+
+# Настройка Jaeger Exporter
 jaeger_exporter = JaegerExporter(
     agent_host_name='jaeger',  # Jaeger host из docker-compose
     agent_port=6831,           # порт Jaeger для UDP
 )
+
+# Создание процессора для отправки трейсингов в Jaeger
 span_processor = BatchSpanProcessor(jaeger_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
 
@@ -24,6 +33,15 @@ FastAPIInstrumentor.instrument_app(app)
 # Инструментирование HTTP-клиентов (например, requests)
 RequestsInstrumentor().instrument()
 
+# Завершение работы (shutdown) при завершении приложения
+@app.on_event("shutdown")
+def shutdown_tracer():
+    try:
+        trace.get_tracer_provider().shutdown()
+    except Exception as e:
+        print(f"Ошибка завершения трейсера: {e}")
+
+        
 @app.get('/')
 def read_main():
     """Func for hello page."""
